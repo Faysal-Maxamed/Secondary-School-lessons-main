4d327dd50112cab515887b9f9418f5e0472bd5c8
Faysal-Maxamed <pheymoha17@gmail.com>
2024-07-20 00:12:56 -0700

    F4Lessons Add 3 Subjects

--- Secondary-School-Lessons/src/Pages/F4Lessons.jsx
+++ Secondary-School-Lessons/src/Pages/F4Lessons.jsx
@@ -4,17 +4,36 @@ import { FaPlayCircle, FaBars } from 'react-icons/fa';
 
 const lessonsData = {
   1: [
-    { id: 1, title: 'Lesson 1', description: 'Description for Lesson 1 Arabic', videoUrl: 'https://www.youtube.com/watch?v=O7OXqETTZRU&list=PLA2sIXNgNKf6z_M5g_s0e2Yte13ic7ten&index=1&pp=iAQB' },
-    { id: 2, title: 'Lesson 2', description: 'Description for Lesson 2 Arabic', videoUrl: 'https://www.youtube.com/watch?v=jdxA0EeAH3o&list=PLA2sIXNgNKf6z_M5g_s0e2Yte13ic7ten&index=2&pp=iAQB' },
-    { id: 3, title: 'Lesson 3', description: 'Description for Lesson 3 Arabic', videoUrl: 'https://www.youtube.com/watch?v=9bXLAbZIhzQ&list=PLA2sIXNgNKf6z_M5g_s0e2Yte13ic7ten&index=3&pp=iAQB' },
-    { id: 4, title: 'Lesson 4', description: 'Description for Lesson 4 Arabic', videoUrl: 'https://www.youtube.com/watch?v=F54keQjGsgs&list=PLA2sIXNgNKf6z_M5g_s0e2Yte13ic7ten&index=4&pp=iAQB' },
-    { id: 5, title: 'Lesson 5', description: 'Description for Lesson 5 Arabic', videoUrl: 'https://www.youtube.com/watch?v=S0MZ6G5ODeY&list=PLA2sIXNgNKf6z_M5g_s0e2Yte13ic7ten&index=5&pp=iAQB' },
-    { id: 6, title: 'Lesson 6', description: 'Description for Lesson 6 Arabic', videoUrl: 'https://www.youtube.com/watch?v=Gq9_KMST4CQ&list=PLA2sIXNgNKf6z_M5g_s0e2Yte13ic7ten&index=6&pp=iAQB' },
+    { id: 1, title: 'Lesson 1', description: 'Description for Lesson 1 Arabic', videoUrl: 'https://www.youtube.com/watch?v=MFCwJB3eW6o&list=PLkl5177d3TqNQUcGDGkbugJpYJ7EENNSA&index=1&pp=iAQB' },
+    { id: 2, title: 'Lesson 2', description: 'Description for Lesson 2 Arabic', videoUrl: 'https://www.youtube.com/watch?v=DoPx7A4DDC4&list=PLkl5177d3TqNQUcGDGkbugJpYJ7EENNSA&index=2&pp=iAQB' },
+    { id: 3, title: 'Lesson 3', description: 'Description for Lesson 3 Arabic', videoUrl: 'https://www.youtube.com/watch?v=I7yyQKLEjqI&list=PLkl5177d3TqNQUcGDGkbugJpYJ7EENNSA&index=3&pp=iAQB' },
+    { id: 4, title: 'Lesson 4', description: 'Description for Lesson 4 Arabic', videoUrl: 'https://www.youtube.com/watch?v=mklb3PkaLp4&list=PLkl5177d3TqNQUcGDGkbugJpYJ7EENNSA&index=4&pp=iAQB' },
+    { id: 5, title: 'Lesson 5', description: 'Description for Lesson 5 Arabic', videoUrl: 'https://www.youtube.com/watch?v=AmsECoGq8kQ&list=PLkl5177d3TqNQUcGDGkbugJpYJ7EENNSA&index=5&pp=iAQB' },
+    { id: 6, title: 'Lesson 6', description: 'Description for Lesson 6 Arabic', videoUrl: 'https://www.youtube.com/watch?v=s8GdWfK2MdY&list=PLkl5177d3TqNQUcGDGkbugJpYJ7EENNSA&index=6&pp=iAQB' },
   ],
   2: [
-    { id: 1, title: 'Lesson 1', description: 'Description for Lesson 1 English', videoUrl: '' },
-    { id: 2, title: 'Lesson 2', description: 'Description for Lesson 2 English', videoUrl: 'https://www.youtube.com/watch?list=PLA2sIXNgNKf5o-qzU7yrC7USJhq__1iDj&v=jndoZU9fpPc&feature=youtu.be' },
-    { id: 3, title: 'Lesson 3', description: 'Description for Lesson 3 English', videoUrl: 'https://www.youtube.com/watch?list=PLA2sIXNgNKf5o-qzU7yrC7USJhq__1iDj&v=jndoZU9fpPc&feature=youtu.be' },
+    { id: 1, title: 'Lesson 1', description: 'Description for Lesson 1 English', videoUrl: 'https://www.youtube.com/watch?v=9qrXT0faoOk&list=PLJMSYg_Bu6JeYfJa5z3BBztC_9Pr8Njns&index=3&pp=iAQB' },
+    { id: 2, title: 'Lesson 2', description: 'Description for Lesson 2 English', videoUrl: 'https://www.youtube.com/watch?v=xQxF278dhYc&list=PLJMSYg_Bu6JeYfJa5z3BBztC_9Pr8Njns&index=1&pp=iAQB' },
+    { id: 3, title: 'Lesson 3', description: 'Description for Lesson 3 English', videoUrl: 'https://www.youtube.com/watch?v=XFT9mBWSBxY&list=PLJMSYg_Bu6JeYfJa5z3BBztC_9Pr8Njns&index=2&pp=iAQB' },
+    { id: 4, title: 'Lesson 4', description: 'Description for Lesson 4 English', videoUrl: 'https://www.youtube.com/watch?v=26uT3fWGQoA&list=PLJMSYg_Bu6JeYfJa5z3BBztC_9Pr8Njns&index=4&pp=iAQB' },
+    { id: 5, title: 'Lesson 5', description: 'Description for Lesson 5 English', videoUrl: 'https://www.youtube.com/watch?v=-j3fc1rSiWU&list=PLJMSYg_Bu6JeYfJa5z3BBztC_9Pr8Njns&index=5&pp=iAQB' },
+    { id: 6, title: 'Lesson 6', description: 'Description for Lesson 6 English', videoUrl: 'https://www.youtube.com/watch?v=AjIZTrEZCSo&list=PLJMSYg_Bu6JeYfJa5z3BBztC_9Pr8Njns&index=6&pp=iAQB' },
+    { id: 7, title: 'Lesson 7', description: 'Description for Lesson 7 English', videoUrl: 'https://www.youtube.com/watch?v=DIsE1bv4rH4&list=PLJMSYg_Bu6JeYfJa5z3BBztC_9Pr8Njns&index=7&pp=iAQB' },
+    { id: 8, title: 'Lesson 8', description: 'Description for Lesson 8 English', videoUrl: 'https://www.youtube.com/watch?v=-g4BnuEt5sQ&list=PLMgOoQLOzMtRW6_KJPEG7877EkIyVHyNJ&index=1&pp=iAQB' },
+    { id: 9, title: 'Lesson 9', description: 'Description for Lesson 9 English', videoUrl: 'https://www.youtube.com/watch?v=osHtfN_brmg&list=PLMgOoQLOzMtRW6_KJPEG7877EkIyVHyNJ&index=2&pp=iAQB' },
+  ],
+  3: [
+    { id: 1, title: 'Lesson 1', description: 'Description for Lesson 1 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=wK9zwIE4njI&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=1&pp=iAQB' },
+    { id: 2, title: 'Lesson 2', description: 'Description for Lesson 2 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=aowzGpPGbBk&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=2&pp=iAQB' },
+    { id: 3, title: 'Lesson 3', description: 'Description for Lesson 3 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=yNz3r9Dcevg&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=3&pp=iAQB' },
+    { id: 4, title: 'Lesson 4', description: 'Description for Lesson 4 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=7FoSqr2GUXM&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=4&pp=iAQB' },
+    { id: 5, title: 'Lesson 5', description: 'Description for Lesson 5 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=wcCHhAJdYPA&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=5&pp=iAQB' },
+    { id: 6, title: 'Lesson 6', description: 'Description for Lesson 6 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=oV9SPuRYIJY&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=6&pp=iAQB' },
+    
+    { id: 7, title: 'Lesson 7', description: 'Description for Lesson 7 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=zpuetkOg0P4&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=7&pp=iAQB' },
+    { id: 8, title: 'Lesson 8', description: 'Description for Lesson 8 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=sf9L07ioZk0&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=8&pp=iAQB' },
+    { id: 9, title: 'Lesson 9', description: 'Description for Lesson 9 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=HnIr_8b4lGg&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=9&pp=iAQB' },
+    { id: 10, title: 'Lesson 10', description: 'Description for Lesson 10 Mathmatics', videoUrl: 'https://www.youtube.com/watch?v=7VRu5YSSOxM&list=PLG4SdbUov5hSBaV6gI0QVp_Qg9uA3QCWK&index=10&pp=iAQB' },
   ],
   // Add similar structure for other courses if needed
 };
@@ -57,7 +76,7 @@ const F4Lessons = () => {
       <div className="flex flex-col md:flex-row">
         {/* Sidebar for Desktop */}
         <div className={`md:w-1/4 bg-gray-200 p-4 rounded-lg ${mobileSidebarVisible ? 'block md:hidden' : 'hidden md:block'}`}>
-          <h4 className="text-xl font-semibold mb-4">Other Lessons in this Course:</h4>
+          <h4 className="text-xl font-semibold mb-12">Other Lessons in this Course:</h4>
           <div className="space-y-2">
             {lessons.map((lesson) => (
               <button
